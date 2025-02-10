059a35a28b76afdb17e63cbe8f3c9f02ddf1856c
HuyHoangEngr <alegiahuy11@gmail.com>
2025-02-10 22:17:53 +0700

    video9. HTTP REQUEST METHOD & SU DUNG DATABASE VOI NODE.JS

---
+++ src/configs/connectDB.js
@@ -0,0 +1,20 @@
+
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic'
+});
+
+// simple query
+// connection.query(
+//     'SELECT * FROM `users`',
+//     function (err, results, fields) {
+//         console.log(">>> Check myspl")
+//         console.log(results); // results contains rows returned by server 
+//     }
+// );
+
+export default connection;
