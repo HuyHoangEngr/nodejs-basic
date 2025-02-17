02c99a4e579d9dab8515039a57d4a4005ff50820
HuyHoangEngr <alegiahuy11@gmail.com>
2025-02-17 15:12:42 +0700

    video12. SU DUNG FORM VA POST REQUEST VOI NODE.JS

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,6 @@ import pool from '../configs/connectDB';
 
 let getHomepage = async (req, res) => {
     const [rows, fields] = await pool.execute('SELECT * FROM users');
-
     return res.render('index.ejs', { dataUser: rows, test: 'abc string test' })
 }
 
@@ -12,6 +11,15 @@ let getDetailPage = async (req, res) => {
     return res.send(JSON.stringify(user))
 }
 
+let createNewUser = async (req, res) => {
+    let { firstName, lastName, email, address } = req.body;
+
+    await pool.execute('insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)',
+        [firstName, lastName, email, address]);
+
+    return res.redirect('/')
+}
+
 module.exports = {
-    getHomepage, getDetailPage
+    getHomepage, getDetailPage, createNewUser
 }
--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ import configViewEngine from './configs/viewEngine';
 import initWebRoute from './routes/web';
 // import connection from './configs/connectDB';
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
-console.log('>>check port: ', port);
 
-//settup view engine
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setup view engine
 configViewEngine(app);
 
 //init web route
